a0427ae7dbcd3a2da190749df8cdf67189e8c4bc
Bamiyemiasulewon <asulewonbamiyemi@gmail.com>
2026-07-10 17:53:57 +0100

    edit

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import { ShoppingBag, Menu, X } from 'lucide-react'
 import { useCart } from '@/lib/cart-context'
